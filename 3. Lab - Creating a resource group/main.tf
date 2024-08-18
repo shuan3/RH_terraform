d8332ec5b2a3039dac4f8958f9d94f6368ffe5eb
Raymond Huang <shanhua18@gmail.com>
2024-08-17 21:51:01 -0400

    udpated

--- 3. Lab - Creating a resource group/main.tf
+++ 3. Lab - Creating a resource group/main.tf
@@ -17,10 +17,10 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
+  subscription_id = ""
   client_id       = "08d82452-5ee9-4b5b-96ea-aec56d9db182"
   client_secret   = "oc38Q~D5P_jTpGSStayL3_3pT2Kqm2olimm2Ja_p"
-  tenant_id       = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
+  tenant_id       = ""
   features {}
 }
 
--- 6. Lab - Uploading a Blob/main.tf
+++ 6. Lab - Uploading a Blob/main.tf
@@ -20,9 +20,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
-  tenant_id = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
-  client_id = "fb3eb97c-8679-488e-a4b4-5fd90299b7a2"
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
   client_secret = ""
   features {}  
 }
